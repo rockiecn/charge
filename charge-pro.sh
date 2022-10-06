d440cc40b289f221fe94fdffb62835571a7c9b85
Your Name <you@example.com>
2022-10-06 22:06:14 +0800

    modify for product

--- charge-pro.sh
+++ charge-pro.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./mefs transfer eth  --endPoint="https://chain.metamemo.one:8501" --sk="474e530fc2aff814e9c4526fe06b4efd2043ffd8e52d67388b057919f0df2bdb" $1 1000gwei 
+./mefs transfer eth  --endPoint="https://chain.metamemo.one:8501" --sk="0a95533a110ee10bdaa902fed92e56f3f7709a532e22b5974c03c0251648a5d4" $1 1000gwei 
 
-./mefs transfer memo  --endPoint="https://chain.metamemo.one:8501" --roleContract="0x0fC98129261dadEc4576f4b052F64354F6f6C634" --sk="474e530fc2aff814e9c4526fe06b4efd2043ffd8e52d67388b057919f0df2bdb" $1 1
+./mefs transfer memo  --endPoint="https://chain.metamemo.one:8501" --roleContract="0x9fB373203AFa1D23fbC35a3E5651A4Ec93558a73" --sk="0a95533a110ee10bdaa902fed92e56f3f7709a532e22b5974c03c0251648a5d4" $1 1
