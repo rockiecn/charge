3e445c8262b4c2f4c7079fc77d1f0ddb3b43c63e
wy <wy@example.com>
2022-10-28 11:31:54 +0800

    update

--- charge-pro.sh
+++ charge-pro.sh
@@ -2,4 +2,4 @@
 
 ./mefs transfer eth  --endPoint="https://chain.metamemo.one:8501" --sk="0a95533a110ee10bdaa902fed92e56f3f7709a532e22b5974c03c0251648a5d4" $1 1000gwei 
 
-./mefs transfer memo  --endPoint="https://chain.metamemo.one:8501" --roleContract="0x9fB373203AFa1D23fbC35a3E5651A4Ec93558a73" --sk="0a95533a110ee10bdaa902fed92e56f3f7709a532e22b5974c03c0251648a5d4" $1 1
+./mefs transfer memo  --endPoint="https://chain.metamemo.one:8501" --version=2 --instanceAddr="0x9fB373203AFa1D23fbC35a3E5651A4Ec93558a73" --sk="0a95533a110ee10bdaa902fed92e56f3f7709a532e22b5974c03c0251648a5d4" $1 1
